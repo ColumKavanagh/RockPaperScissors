9059a4b55f58bdadf8f39eb31a54a493256a6a72
Colum Kavanagh <colum.j.kavanagh@gmail.com>
2021-11-08 20:49:23 +0000

    working code and images

---
+++ script.js
@@ -0,0 +1,39 @@
+var playerPick; //this has to be declared outside the function as the first thing the 'onclick' event does in the html is set this varable to either 1, 2, or 3.
+function runGame(){
+    var randomNum = Math.floor(Math.random() * 3) + 1;//computer picks a number from 1 to 3
+    if (randomNum==1){
+        computerChoice="Rock";
+        }else if (randomNum==2){
+            computerChoice="Paper";
+        }else if(randomNum==3){
+            computerChoice="Scissors";
+            }
+    if (playerPick==1){
+        playerChoice="Rock";
+        }else if (playerPick==2){
+            playerChoice="Paper";
+        }else if (playerPick==3){
+            playerChoice="Scissors";
+            }
+    if(playerPick==randomNum){ //draw
+                document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so it was a draw!";
+        }else if (playerPick==1){ //ROCK
+            if(randomNum==2){ //paper
+                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you lose!";
+                }else if(randomNum==3){ //scissors     
+                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you win!";
+                }
+        }else if(playerPick==2){ //PAPER
+            if(randomNum==1){ //rock
+                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you win!";
+                }else if(randomNum==3){ //scissors
+                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you lose!";
+                }
+        }else if(playerPick==3){ //SCISSORS
+            if(randomNum==1){ //rock
+                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you lose!";
+                }else if(randomNum==2){ //paper
+                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you win!";
+                }
+        }
+}

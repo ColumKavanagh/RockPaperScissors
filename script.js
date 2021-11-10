36d58ed949ff7309933748ecf032678933bf7663
Colum Kavanagh <colum.j.kavanagh@gmail.com>
2021-11-10 09:44:16 +0000

    streamlined js using fewer variables

--- script.js
+++ script.js
@@ -1,41 +1,33 @@
 var playerPick; //this has to be declared outside the function as the first thing that the 'onclick' event on the image in the html does is set this variable to either 1, 2, or 3.
 function runGame(){
     var randomNum = Math.floor(Math.random() * 3) + 1;//computer picks a number from 1 to 3
-    var computerChoice;
-    var playerChoice;
+    var computerPick;
     if (randomNum==1){
-        computerChoice="Rock";
+        computerPick="Rock";
         }else if (randomNum==2){
-            computerChoice="Paper";
+            computerPick="Paper";
         }else if(randomNum==3){
-            computerChoice="Scissors";
+            computerPick="Scissors";
             }
-    if (playerPick==1){
-        playerChoice="Rock";
-        }else if (playerPick==2){
-            playerChoice="Paper";
-        }else if (playerPick==3){
-            playerChoice="Scissors";
-            }
-    if(playerPick==randomNum){ //draw
-                document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so it was a draw!";
-        }else if (playerPick==1){ //ROCK
-            if(randomNum==2){ //paper
-                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you lose!";
-                }else if(randomNum==3){ //scissors     
-                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you win!";
+    if(playerPick==computerPick){ //draw
+                document.getElementById("gameResult").innerHTML="You picked "+playerPick+". The computer picked "+computerPick+", so it was a draw!";
+        }else if (playerPick=="Rock"){ //ROCK
+            if(computerPick=="Paper"){ //paper
+                    document.getElementById("gameResult").innerHTML="You picked "+playerPick+". The computer picked "+computerPick+", so you lose!";
+                }else if(computerPick=="Scissors"){ //scissors     
+                    document.getElementById("gameResult").innerHTML="You picked "+playerPick+". The computer picked "+computerPick+", so you win!";
                 }
-        }else if(playerPick==2){ //PAPER
-            if(randomNum==1){ //rock
-                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you win!";
-                }else if(randomNum==3){ //scissors
-                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you lose!";
+        }else if(playerPick=="Paper"){ //PAPER
+            if(computerPick=="Rock"){ //rock
+                    document.getElementById("gameResult").innerHTML="You picked "+playerPick+". The computer picked "+computerPick+", so you win!";
+                }else if(computerPick=="Scissors"){ //scissors
+                    document.getElementById("gameResult").innerHTML="You picked "+playerPick+". The computer picked "+computerPick+", so you lose!";
                 }
-        }else if(playerPick==3){ //SCISSORS
-            if(randomNum==1){ //rock
-                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you lose!";
-                }else if(randomNum==2){ //paper
-                    document.getElementById("gameResult").innerHTML="You picked "+playerChoice+". The computer picked "+computerChoice+", so you win!";
+        }else if(playerPick=="Scissors"){ //SCISSORS
+            if(computerPick=="Rock"){ //rock
+                    document.getElementById("gameResult").innerHTML="You picked "+playerPick+". The computer picked "+computerPick+", so you lose!";
+                }else if(computerPick=="Paper"){ //paper
+                    document.getElementById("gameResult").innerHTML="You picked "+playerPick+". The computer picked "+computerPick+", so you win!";
                 }
         }
 }
